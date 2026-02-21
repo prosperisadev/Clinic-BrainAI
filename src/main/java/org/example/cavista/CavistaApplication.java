61554a840466343e849224f9e8ebdeed4fd0ab23
Kene Ijeh <keneijeh760@gmail.com>
2026-02-21 11:26:22 +0100

    first commit

---
+++ src/main/java/org/example/cavista/CavistaApplication.java
@@ -0,0 +1,13 @@
+package org.example.cavista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CavistaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CavistaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cavista/CavistaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cavista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CavistaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
